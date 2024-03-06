4777649aec3eab7f5d90d1ac8d1bd401bfab8171
unknown <anhkhoacttv@gmail.com>
2024-03-06 22:07:46 +0700

    first commit

---
+++ components/route/index.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>route component</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>Index word<Hello></Hello></div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
